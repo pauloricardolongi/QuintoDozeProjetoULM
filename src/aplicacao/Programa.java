37ac73c056d10f8e0ad4d09074e4bda4d957b302
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-04 02:01:49 -0300

    projeto com total de usuários

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,24 +1,46 @@
 package aplicacao;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
-import entidades.Produto;
+import entities.LogEntry;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		//comparar como o TreeSet compara os elementos
-		//classe Produto implements Comparable
 		
-		Set<Produto> set = new TreeSet<>();
-		set.add(new Produto("TV", 900.0));
-		set.add(new Produto("Notebook", 1200.0));
-		set.add(new Produto("Tablet", 400.0));
+		Scanner sc = new Scanner(System.in);
 		
-		for (Produto p : set) {
-		System.out.println(p);
+		System.out.print("Entre com o caminho do arquivo path: ");
+		String path = sc.nextLine();
+		
+		try ( BufferedReader br = new BufferedReader(new FileReader(path))) {
+			Set<LogEntry> set = new HashSet<>();
+			
+			String line = br.readLine();
+			while (line != null){
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moments = Date.from(Instant.parse(fields[1]));
+				
+				set.add(new LogEntry(username, moments));
+				line = br.readLine();
+			}
+			System.out.println("Total de usuários: " + set.size());
 		}
+		
+		catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+	
 	}
+		sc.close();
 
 }
+}
+
